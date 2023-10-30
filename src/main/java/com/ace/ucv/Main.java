44ab56bde8e0e3422fe3aab089e39a83a5775b9b
Clara2605 <bondrescu.clara.h5i@student.ucv.ro>
2023-10-30 22:03:54 +0200

    added table conected to DatabaseManager

--- src/main/java/com/ace/ucv/Main.java
+++ src/main/java/com/ace/ucv/Main.java
@@ -1,11 +1,15 @@
 package com.ace.ucv;
 
+import com.ace.ucv.db.CreateTable;
 import com.ace.ucv.db.DatabaseManager;
 import com.ace.ucv.model.Patient;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.collections.FXCollections;
@@ -13,12 +17,14 @@ import javafx.collections.ObservableList;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main extends Application {
     private ObservableList<Patient> patients = FXCollections.observableArrayList();
     private TextField nameField, ageField, fieldOfWorkField;
-    private ListView<Patient> patientListView;
+    private TableView<Patient> patientTableView;
+    private Button addButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -36,46 +42,117 @@ public class Main extends Application {
         GridPane.setConstraints(nameLabel, 0, 0);
         nameField = new TextField();
         GridPane.setConstraints(nameField, 1, 0);
+        ImageView nameCheck = new ImageView(); // Crearea ImageView pentru Name
+        nameCheck.setFitWidth(20);
+        nameCheck.setFitHeight(20);
+        GridPane.setConstraints(nameCheck, 2, 0);
 
         Label ageLabel = new Label("Age:");
         GridPane.setConstraints(ageLabel, 0, 1);
         ageField = new TextField();
         GridPane.setConstraints(ageField, 1, 1);
+        ImageView ageCheck = new ImageView(); // Crearea ImageView pentru Age
+        ageCheck.setFitWidth(20);
+        ageCheck.setFitHeight(20);
+        GridPane.setConstraints(ageCheck, 2, 1);
 
         Label fieldOfWorkLabel = new Label("Field of Work:");
         GridPane.setConstraints(fieldOfWorkLabel, 0, 2);
         fieldOfWorkField = new TextField();
         GridPane.setConstraints(fieldOfWorkField, 1, 2);
+        ImageView fieldOfWorkCheck = new ImageView(); // Crearea ImageView pentru Field of Work
+        fieldOfWorkCheck.setFitWidth(20);
+        fieldOfWorkCheck.setFitHeight(20);
+        GridPane.setConstraints(fieldOfWorkCheck, 2, 2);
 
-        Button addButton = new Button("Add Patient");
-        GridPane.setConstraints(addButton, 1, 3);
-        addButton.setOnAction(e -> addPatient());
+        addButton = new Button("Add Patient");
+        GridPane.setConstraints(addButton, 0, 3);
+        GridPane.setColumnSpan(addButton, 2); // Lărgirea butonului pe două coloane
+        addButton.setDisable(true); // Dezactivează butonul inițial
 
-        Label patientListLabel = new Label("Patient List:");
-        GridPane.setConstraints(patientListLabel, 0, 4);
-        patientListView = new ListView<>();
-        GridPane.setConstraints(patientListView, 1, 4);
-        patientListView.setItems(patients);
-
-        patientListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                showPatientDetails(newValue);
+        addButton.setOnAction(e -> {
+            if (validateFields()) {
+                addPatient();
             }
         });
 
-        GridPane.setColumnSpan(patientListView, 2);
+        nameField.textProperty().addListener((observable, oldValue, newValue) -> {
+            updateAddButtonState(nameCheck, newValue); // Actualizează starea butonului și imaginea pentru Name
+        });
+
+        ageField.textProperty().addListener((observable, oldValue, newValue) -> {
+            updateAddButtonState(ageCheck, newValue); // Actualizează starea butonului și imaginea pentru Age
+        });
+
+        fieldOfWorkField.textProperty().addListener((observable, oldValue, newValue) -> {
+            updateAddButtonState(fieldOfWorkCheck, newValue); // Actualizează starea butonului și imaginea pentru Field of Work
+        });
+
+        patientTableView = new TableView<>();
+        TableColumn<Patient, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Patient, Integer> ageColumn = new TableColumn<>("Age");
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+        TableColumn<Patient, String> fieldOfWorkColumn = new TableColumn<>("Field of Work");
+        fieldOfWorkColumn.setCellValueFactory(new PropertyValueFactory<>("fieldOfWork"));
+
+        patientTableView.getColumns().addAll(nameColumn, ageColumn, fieldOfWorkColumn);
+        patientTableView.setItems(patients);
+        GridPane.setConstraints(patientTableView, 0, 4);
+        GridPane.setConstraints(patientTableView, 1, 4); // Lărgirea tabelului pe două coloane
 
         grid.getChildren().addAll(
-                nameLabel, nameField,
-                ageLabel, ageField,
-                fieldOfWorkLabel, fieldOfWorkField,
+                nameLabel, nameField, nameCheck,
+                ageLabel, ageField, ageCheck,
+                fieldOfWorkLabel, fieldOfWorkField, fieldOfWorkCheck,
                 addButton,
-                patientListLabel, patientListView
+                patientTableView
         );
 
-        Scene scene = new Scene(grid, 400, 300);
+        Scene scene = new Scene(grid, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // Crează tabela la începutul aplicației
+        CreateTable.createTable();
+        loadPatients();
+    }
+
+    private boolean validateFields() {
+        String name = nameField.getText().trim();
+        String age = ageField.getText().trim();
+        String fieldOfWork = fieldOfWorkField.getText().trim();
+
+        boolean validName = !name.isEmpty() && name.matches("[a-zA-Z ]+");
+        boolean validAge = !age.isEmpty() && age.matches("\\d+");
+        boolean validFieldOfWork = !fieldOfWork.isEmpty() && fieldOfWork.matches("[a-zA-Z ]+");
+
+        return validName && validAge && validFieldOfWork;
+    }
+
+    private void updateAddButtonState(ImageView imageView, String newValue) {
+        boolean isValid = !newValue.isEmpty() && newValue.matches("[a-zA-Z ]+");
+        if (isValid) {
+            imageView.setImage(new Image("/green-checkbox.jpg")); // Încarcă imaginea corectă
+        } else {
+            imageView.setImage(null); // Dacă textul nu este valid, nu afișăm nimic
+        }
+        if (ageField.getText().isEmpty() || !ageField.getText().matches("\\d+")) {
+            imageView.setImage(null); // Șterge imaginea dacă vârsta nu este un număr valid
+        } else {
+            int age = Integer.parseInt(ageField.getText());
+            if (age < 0 || age > 100) {
+                imageView.setImage(null); // Șterge imaginea dacă vârsta nu se află în intervalul specificat
+            }
+        }
+        updateAddButtonState();
+    }
+
+
+    private void updateAddButtonState() {
+        addButton.setDisable(!validateFields());
     }
 
     private void addPatient() {
@@ -101,11 +178,20 @@ public class Main extends Application {
         fieldOfWorkField.clear();
     }
 
-    private void showPatientDetails(Patient patient) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Patient Details");
-        alert.setHeaderText(null);
-        alert.setContentText("Name: " + patient.getName() + "\nAge: " + patient.getAge() + "\nField of Work: " + patient.getFieldOfWork());
-        alert.showAndWait();
+    private void loadPatients() {
+        patients.clear();
+        try (Connection connection = DatabaseManager.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM patients");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String fieldOfWork = resultSet.getString("field_of_work");
+                Patient patient = new Patient(name, age, fieldOfWork);
+                patients.add(patient);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/ace/ucv/db/DatabaseManager.java
+++ src/main/java/com/ace/ucv/db/DatabaseManager.java
@@ -1,10 +1,11 @@
 package com.ace.ucv.db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String DATABASE_URL = "jdbc:sqlite:D:\\Anul1 IS1.1\\MISC\\MSIC_PROIECT\\src\\main\\resources\\database\\database.db"; // Replace with your database path
+    private static final String DATABASE_URL = "jdbc:sqlite:D:/Anul1 IS1.1/MISC/MSIC_PROIECT/src/main/resources/database/database.db"; // Schimbă cu calea către baza ta de date
 
     public static Connection connect() {
         try {
